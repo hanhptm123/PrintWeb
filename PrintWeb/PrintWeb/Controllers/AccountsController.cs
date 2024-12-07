b8addd22cae01b1683152090f92cfdea909b7a1e
hanhptm123 <studyhanh@gmail.com>
2024-12-07 10:29:48 +0700

    feat(auth): Add Login feature

---
+++ PrintWeb/PrintWeb/Controllers/AccountsController.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrintWeb.Data;
+
+namespace PrintWeb.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly PrintwebContext _context;
+
+        public AccountsController(PrintwebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts
+        public async Task<IActionResult> Index()
+        {
+            var printwebContext = _context.Accounts.Include(a => a.AccountNavigation);
+            return View(await printwebContext.ToListAsync());
+        }
+
+        // GET: Accounts/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.AccountNavigation)
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Accounts/Create
+        public IActionResult Create()
+        {
+            ViewData["AccountId"] = new SelectList(_context.Students, "StudentId", "StudentId");
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AccountId,Password")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(account);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Students, "StudentId", "StudentId", account.AccountId);
+            return View(account);
+        }
+
+        // GET: Accounts/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            ViewData["AccountId"] = new SelectList(_context.Students, "StudentId", "StudentId", account.AccountId);
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("AccountId,Password")] Account account)
+        {
+            if (id != account.AccountId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.AccountId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AccountId"] = new SelectList(_context.Students, "StudentId", "StudentId", account.AccountId);
+            return View(account);
+        }
+
+        // GET: Accounts/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .Include(a => a.AccountNavigation)
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(string id)
+        {
+            return _context.Accounts.Any(e => e.AccountId == id);
+        }
+        //Feature:Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync();
+            return View("Login");
+        }
+        [HttpPost]
+        public IActionResult Login(string accountid, string password)
+        {
+            var account = _context.Accounts.Where(t => t.AccountId == accountid && t.Password == password).FirstOrDefault<Account>();
+
+            if (account == null)
+            {
+                TempData["ErrorMessage"] = "Invalid username or password.";
+                return RedirectToAction("Login");
+            }
+            TempData["SuccessMessage"] = "Login successful! Redirecting...";
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, account.AccountId),
+                new Claim(ClaimTypes.Role, account.Role),
+            };
+            var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity));
+            return RedirectToAction("Login");
+        }
+    }
+}
